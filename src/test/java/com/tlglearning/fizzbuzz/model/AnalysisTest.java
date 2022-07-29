5cc1c162547e1dd0d7c2e9bf2311ab5483e13e42
scaffidic <cp.scaffidi@gmail.com>
2022-07-29 10:42:43 -0700

    InvalidInvocation is local class

--- src/test/java/com/tlglearning/fizzbuzz/model/AnalysisTest.java
+++ src/test/java/com/tlglearning/fizzbuzz/model/AnalysisTest.java
@@ -52,21 +52,21 @@ class AnalysisTest {
   @ParameterizedTest
   @ValueSource(ints = {-1, -3, -5, -15})
   void analyze_negative(int value) {
+    class InvalidInvocation implements Executable {
+
+      private final int value;
+
+
+      public InvalidInvocation(int value) {
+        this.value = value;
+      }
+
+      @Override
+      public void execute() throws Throwable {
+        analysis.analyze(value);
+      }
+    }
     assertThrows(IllegalArgumentException.class, new InvalidInvocation(value));
   }
 
-  class InvalidInvocation implements Executable {
-
-    private final int value;
-
-
-    public InvalidInvocation(int value) {
-      this.value = value;
-    }
-
-    @Override
-    public void execute() throws Throwable {
-      analysis.analyze(value);
-    }
-  }
 }
