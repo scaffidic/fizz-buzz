8d108ffcd6760a8f76e2c56885fd065d7f68ae20
scaffidic <cp.scaffidi@gmail.com>
2022-07-29 10:56:09 -0700

    InvalidInvocation is now anonymous

--- src/test/java/com/tlglearning/fizzbuzz/model/AnalysisTest.java
+++ src/test/java/com/tlglearning/fizzbuzz/model/AnalysisTest.java
@@ -52,21 +52,14 @@ class AnalysisTest {
   @ParameterizedTest
   @ValueSource(ints = {-1, -3, -5, -15})
   void analyze_negative(int value) {
-    class InvalidInvocation implements Executable {
-
-      private final int value;
-
-
-      public InvalidInvocation(int value) {
-        this.value = value;
-      }
+    Executable invalidInvocation = new Executable() {
 
       @Override
       public void execute() throws Throwable {
         analysis.analyze(value);
       }
-    }
-    assertThrows(IllegalArgumentException.class, new InvalidInvocation(value));
+    };
+    assertThrows(IllegalArgumentException.class, invalidInvocation);
   }
 
 }
